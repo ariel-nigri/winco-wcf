4eb719b04620f18578a1e534f0ad0a8c70709868
nigri <nigri@winco.com.br>
2020-07-03 18:51:44 -0300

    worker_hostname -> worker_frontend

--- common/wcf_config.php
+++ common/wcf_config.php
@@ -32,7 +32,7 @@ function wcf_create_or_update(&$inst_id, $inst_caps, $inst_name, $inst_lang, $us
                 }
             }
             // get our own worker_seq;
-            $worker = Workers::find($dbconn, ['worker_hostname' => $my_worker_hostname, 'worker_active' => true]);
+            $worker = Workers::find($dbconn, ['worker_frontend' => $my_worker_hostname, 'worker_active' => true]);
             if (!$worker->valid) {
                 $error = 'Cannot find worker or it is not active';
                 break;
