74fe7720d4747651b5874bad3275545f948e0e92
nigri <nigri@winco.com.br>
2021-04-20 22:14:45 -0300

    MOD: browser based activation fixed

--- pub/wsvc/activatereg.php
+++ pub/wsvc/activatereg.php
@@ -1,7 +1,9 @@
 <?php
 
+// test string
+// https://ariel-dev.talkmanager.net/wsvc/activatereg.php?version=4.4.4&sid=12345&machine=test&user=ariel&64bits=1
 // All we must do is to include the version in the instance.
 $wcf_base = dirname(dirname(__DIR__));
-$curr_ver = trim(file_get_contents("{$wcf_base}/current_version_WTM.cfg"));
+$curr_ver = trim(file_get_contents("{$wcf_base}/config/current_version_WTM.cfg"));
 
 require "/home/instances/versions/{$curr_ver}/pub/activatereg.php";
