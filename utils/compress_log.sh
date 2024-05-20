0015ac838f610d9819520cc997991dd0e1deae0a
Ariel Nigri <nigri@winco.com.br>
2024-05-20 18:42:52 -0300

    New backup system phasing in

--- utils/compress_log.sh
+++ utils/compress_log.sh
@@ -20,13 +20,24 @@ TODAY=$(date +%s)
 BCK_DATE=$((TODAY-90*24*3600))
 BCK_DATE_STR=$(date +%Y%m%d -d @$BCK_DATE)
 
-touch -t ${BCK_DATE_STR}0000 /tmp/backup_reference
+if [ "$1" = all ]; then
+	instances="/home/instances/*"
+elif [ ! -z $1 ]; then
+	while [ "$1" != "" ]; do
+		instances="$instances /home/instances/$1"
+		shift
+	done
+else
+	echo "usage: compress_log.sh [all|inst ..]" >&2
+	exit 1
+fi
 
+touch -t ${BCK_DATE_STR}0000 /tmp/backup_reference
 ls -l /tmp/backup_reference
-for DIR in /home/instances/*; do
-	echo $LOG_INFO $DIR
+
+for DIR in $instances; do
+	echo $DIR
 	if [ -d  $DIR/var/winco/logs ]; then
 	   find $DIR/var/winco/logs -name '*.LOG_*.LOG' ! -newer /tmp/backup_reference -exec bash -c 'mvcompress "$0"' {} \;
 	fi
 done
-
